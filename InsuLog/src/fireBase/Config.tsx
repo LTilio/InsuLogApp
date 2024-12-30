228171b6cc05a916e96e78a945bddb5d0833f5b2
LTilio <leandro.tilio@gmail.com>
2024-12-29 23:30:47 -0300

    Fix: Build APK

--- InsuLog/src/fireBase/Config.tsx
+++ InsuLog/src/fireBase/Config.tsx
@@ -1,16 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { API_KEY, APP_ID, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID } from "@env";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  // apiKey: process.env.EXPO_PUBLIC_API_KEY,
-  apiKey: API_KEY,
-  authDomain: AUTH_DOMAIN,
-  projectId: PROJECT_ID,
-  storageBucket: STORAGE_BUCKET,
-  messagingSenderId: MESSAGING_SENDER_ID,
-  appId: APP_ID,
+  apiKey: process.env.EXPO_PUBLIC_INSULOG_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_INSULOG_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_INSULOG_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_INSULOG_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_INSULOG_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_INSULOG_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
